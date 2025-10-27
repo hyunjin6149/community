755a1d565410c13bf7e7e967a59fbc702b27126a
hyunjin <202400703@hufs.ac.kr>
2025-10-27 13:01:54 +0900

    final commit

---
+++ .vscode/Text.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, View } from "react-native";
+
+interface TextProps {}
+
+const Text = ({}: TextProps) => {
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default Text;
+console.log("");
