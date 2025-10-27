bccc47224b34dcff32b269506551773a9b656004
hyunjin <202400703@hufs.ac.kr>
2025-10-28 03:02:31 +0900

    week2,3 -2

--- .vscode/Text.tsx
+++ .vscode/Text.tsx
@@ -1,12 +1,8 @@
 console.log("");
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 
 interface TextProps {}
 
 const Text = ({}: TextProps) => {
   return <View></View>;
 };
-
-const styles = StyleSheet.create({});
-
-export default Text;
